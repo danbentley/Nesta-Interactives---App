7d868a6241f34d9969520c41cdd5fc5ebf2e0f95
Dan Bentley <danbentley9@gmail.com>
2013-04-08 13:19:11 +0100

    Got dragging working!

--- js/game.js
+++ js/game.js
@@ -1,18 +1,46 @@
 var canvasElem = document.getElementById("game");
 var world = boxbox.createWorld(canvasElem);
  
-world.createEntity({
+var player = world.createEntity({
   name: "player",
   shape: "circle",
   radius: 1,
   image: "img/pig.png",
   imageStretchToFit: true,
   density: 4,
-  x: 2,
-  onKeyDown: function(e) {
-    this.applyImpulse(200, 60);
-  }
+  x: 2
 });
+
+var clickOrigin;
+$(window).on('mousedown', function(e) {
+    clickOrigin = {
+        x:e.offsetX,
+        y:e.offsetY
+    };
+});
+$(window).on('mouseup', function(e) {
+    var dragDistance = clickOrigin.x + e.offsetX;
+
+    var degree = getAngle({
+        x:e.pageX,
+        y:e.pageY,
+    }, clickOrigin);
+
+    player.applyImpulse(dragDistance, degree);
+});
+
+function getAngle(startPosition, endPosition) {
+    var calcAngle = Math.atan2(startPosition.x - endPosition.x, startPosition.y - endPosition.y) * (180 / Math.PI);	
+    // To atan2 returns a range of -180 to 180 which is perfect
+    // for CSS3 rotation but not here where we'd prefer a range
+    // between 0 and 360
+    if(calcAngle < 0) {
+        calcAngle = Math.abs(calcAngle);
+    } else {
+        calcAngle = 360 - calcAngle;		
+    }
+    return calcAngle;
+}
  
 world.createEntity({
   name: "ground",
