df236faecd9efaa4e3eebde84434dbaa052d360f
Dan Bentley <danbentley9@gmail.com>
2013-04-22 16:23:02 +0100

    Moving require.js config to main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+require.config({
+	shim: {
+		'boxbox': ['box2dweb']
+	},
+	paths: {
+		'boxbox': 'lib/boxbox/boxbox.min',
+		'box2dweb': 'lib/boxbox/Box2dWeb-2.1.a.3.min'
+	}
+});
+
 require(['src/app'], function(app) {
 	app.init();
 });
